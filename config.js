40a6cafa00b4a772bac52c52b58aa57e5b090710
bandciao7-gif <bandciao7@gmail.com>
2025-09-09 21:27:13 -0400

    Update config.js

--- config.js
+++ config.js
@@ -5,9 +5,9 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "SHABAN-MD~eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiOEErQWVKM2NFakxtT0VucXVJbkZ5VnlLbTRPVWUyVUFCa1RkeWYyY1NYTT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoicWIvbXpkY0pnazh5UHVtY2tIU3Nma1I1WXBESm5jY1dHWGtYNjY5bTcwUT0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJlRkZYYnFYMUVoVENKaGJDM1AxQlQ0cjhVZFBBRm5iYjNDQVdOWHFWZDNRPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiIzaVM4OGw2YjhqOE82b21HVFphSmpFNUl6M3czUTNCU2FaeVVScUZIakVFPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IlFKNkRNYnpOb2RMcmh3SUJpdkd0NS91enB0c0pFQzVJV0toWURRbUV6RUU9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6InY4eVJ4akJ1eHBnaG9EUTdpRWVUZC9EMi9FZzNwVjdRekgzeVhNVDdPVFk9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiME93TFRSUkowRU1ZQ016em9BMjI3OEE4eHVna0xOaXB3NGtKRm9oVUptQT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoib0hkb1ZDQ1pMdkZNdHVzSXd6QytsVFZ2MFN3R2tRM2ltSjB0aEJDMDBRTT0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6Ilk4bVNhZnJQbkhTakkrSitnSkxDTURUeDAvWGZkcnk0eHVQK1N2UjNOSUtURWRFOXlpeVN5dlBvOS9OY0xnOHliZkdId09VZk9jMGkxOEpwNGo5NUJBPT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6OTQsImFkdlNlY3JldEtleSI6IlNNQmtVVVVhaUVwRmp6NmVCbXRyN0dHVU9ianBqKzZSUUloYURmUTV2Y1E9IiwicHJvY2Vzc2VkSGlzdG9yeU1lc3NhZ2VzIjpbXSwibmV4dFByZUtleUlkIjozMSwiZmlyc3RVbnVwbG9hZGVkUHJlS2V5SWQiOjMxLCJhY2NvdW50U3luY0NvdW50ZXIiOjAsImFjY291bnRTZXR0aW5ncyI6eyJ1bmFyY2hpdmVDaGF0cyI6ZmFsc2V9LCJyZWdpc3RlcmVkIjp0cnVlLCJwYWlyaW5nQ29kZSI6IlY4MUdFRDQyIiwibWUiOnsiaWQiOiIxODI5Mjc0ODExMjo5M0BzLndoYXRzYXBwLm5ldCIsImxpZCI6IjIzODAyMzM0NjI0NTg3Nzo5M0BsaWQifSwiYWNjb3VudCI6eyJkZXRhaWxzIjoiQ0pxNjg3b0NFTXltZzhZR0dBRWdBQ2dBIiwiYWNjb3VudFNpZ25hdHVyZUtleSI6ImxJbFlGcG1BWmpMa1M2eUJrZ2piOUxKMUZMdmp3NFhac2RXSmxaZXY3Q009IiwiYWNjb3VudFNpZ25hdHVyZSI6IkJvQWw3NFJZT0x3VlRTdGluM1R6TGZ2UjFmUUtjT0xZQjFHcXV0TkxOWmd5QXQvcjhtQzVLRXFlSzVzRVpLb1hxRCs3Qnd0VFJXcjBhRlVZNFlEcUFBPT0iLCJkZXZpY2VTaWduYXR1cmUiOiJONEhNbkl0ZmRYQU9XYmFhNTk4dTkwaXFPRkRSd0w5bGdOVHVBZ1dOUHY3ZW0zVmlqcnJHU1JaRVJNYzUxR3dRZyt2NlRaR3hCaTJ5d005bmU3STVBdz09In0sInNpZ25hbElkZW50aXRpZXMiOlt7ImlkZW50aWZpZXIiOnsibmFtZSI6IjE4MjkyNzQ4MTEyOjkzQHMud2hhdHNhcHAubmV0IiwiZGV2aWNlSWQiOjB9LCJpZGVudGlmaWVyS2V5Ijp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiQlpTSldCYVpnR1l5NUV1c2daSUkyL1N5ZFJTNzQ4T0YyYkhWaVpXWHIrd2oifX1dLCJwbGF0Zm9ybSI6ImFuZHJvaWQiLCJyb3V0aW5nSW5mbyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkNBSUlEUT09In0sImxhc3RBY2NvdW50U3luY1RpbWVzdGFtcCI6MTc1NzQ2NzQ3MSwibGFzdFByb3BIYXNoIjoiMlY3N3FVIiwibXlBcHBTdGF0ZUtleUlkIjoiQUFBQUFMWGUifQ==",
 // add your Session Id 
-AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
+AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "false",
 // make true or false status auto seen
 AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
 // make true if you want auto reply on status 
@@ -33,7 +33,7 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "923059395959",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "18292748112",
 // add your bot owner number
 OWNER_NAME: process.env.OWNER_NAME || "MR-SHABAN",
 // add bot owner name
@@ -49,9 +49,9 @@ AUTO_REACT: process.env.AUTO_REACT || "false",
 // make this true or false for auto react on all msgs
 ANTI_BAD: process.env.ANTI_BAD || "false",
 // false or true for anti bad words  
-MODE: process.env.MODE || "public",
+MODE: process.env.MODE || "private",
 // make bot public-private-inbox-group 
-ANTI_LINK: process.env.ANTI_LINK || "true",
+ANTI_LINK: process.env.ANTI_LINK || "false",
 // make anti link true,false for groups 
 AUTO_VOICE: process.env.AUTO_VOICE || "false",
 // make true for send automatic voices
@@ -77,6 +77,6 @@ ANTI_DELETE: process.env.ANTI_DELETE || "false",
 // set true false for anti delete
 ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "inbox", 
 // change it to 'same' if you want to resend deleted message in same chat
-AUTO_RECORDING: process.env.AUTO_RECORDING || "false"
+AUTO_RECORDING: process.env.AUTO_RECORDING || "true"
 // make it true for auto recoding 
 };
